8e6dda30f167dbe563036e94947dd4973047117d
Henok Wondasan <16henok@gmail.com>
2019-09-12 19:29:26 -0500

    Inital commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const sqlHand = require("./util/sql_handler.js");
+const config = require("./config.json");
+const Discord = require("discord.js");
+const colors = require("colors");
+const fs = require("fs");
+
+const sqlite3 = require('sqlite3').verbose();
+const client = new Discord.Client();
+
+fs.readdir("./commands/", (err, files) =>
+{
+	if(err)
+		client.emit("error", err);
+
+	let jsfile = files.filter(f => f.split(".").pop() === "js");
+	if(jsfile.length <= 0)
+	{//
+		console.log("\n> Unable to find commands.");
+		return;
+	}
+
+	console.log("\n==== COMMANDS ====");
+	console.log(`${jsfile.length} commands found`);
+	console.log("==================\n");
+	jsfile.forEach((f, i) => { let props = require(`./commands/${f}`); });
+});
+
+client.on("ready", async () =>
+{
+	console.log("> I'm now ONLINE!\n".bold.green);
+	client.user.setActivity("@" + client.user.username + " for help");
+
+	//Actions needed after bot starts up
+	await startup();
+});
+
+client.on("guildCreate", async guild =>
+{
+	console.log("I've been connected to a new guild:\n" + guild.name + "\n" + guild.id);
+});
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+// client.on("debug", (e) => console.info(e));
+
+client.on("message", async message =>
+{
+	if(message.channel.type === "dm") return;
+	if(message.author.bot) return;
+
+	let guildInfo = await sqlHand.getData(client, './SQL/guildsDB.db3', 'data', 'guild', Number(message.guild.id));
+	let args = message.content.slice(guildInfo.prefix.length).trim().split(' ');
+	let cmd = args.shift().toLowerCase();
+
+	//If they happened to ping the bot
+	if(message.content == (client.user))
+		return message.channel.send("For further help, please feel free to use **`" + guildInfo.prefix + "help`** for a complete rundown of all my commands.\n\n**Guild prefix:** `" + guildInfo.prefix + "`");
+
+	//Not their prefix, RUN AWAY!
+	if(!message.content.startsWith(guildInfo.prefix)) return;
+
+	try
+	{
+		let cmdFile = require(`./commands/${cmd}.js`);
+		cmdFile.run(client, message, args);
+	} catch(e) { }
+});
+
+async function startup()
+{
+	//Directory check
+	await dirCheck();
+
+	//exports.createdb = function(client, sqlDir, table, query, uniqueValue)
+	const playerDB = "playerdb";
+	await sqlHand.createdb(client, `./SQL/${playerDB}.db3`, "data", config.sql_playerDBQuery,"id");
+
+	const guildDB = "guildDB";
+	await sqlHand.createdb(client, `./SQL/${guildDB}.db3`, "data", config.sql_guildQuery, "id");
+}
+
+async function dirCheck()
+{
+	let myGuilds = client.guilds.array();
+	if (!fs.existsSync("./SQL")) { fs.mkdirSync("./SQL"); }
+
+	return;
+}
+
+
+client.login(config.token);
---
+++ util/sql_handler.js
@@ -0,0 +1,166 @@
+const SQLite = require("sqlite3");
+const fs = require("fs");
+
+/*
+sqlDir - SQL directory
+table - the name of the table within the desired database
+identifier - the column used to seperate unique entries
+index - the desired entry to be returned
+*/
+exports.getData = async function(client, sqlDir, table, identifier, index)
+{
+    return new Promise(async (resolve, reject) =>
+    {
+        let db = new SQLite.Database(sqlDir);
+        try
+        {
+            let dataEntry = undefined;
+            let returnData = undefined;
+
+            db.serialize(function()
+            {
+                let query = db.prepare(`SELECT * FROM ${table} WHERE ${identifier} = ?`);
+                dataEntry = new Promise((resolve, reject) => {
+                    query.get(index, function(err, data) { resolve(data); });
+                });
+
+                query.finalize();
+            });
+
+            db.close();
+            await dataEntry.then(data => returnData = data);
+            resolve(returnData);
+        }
+        catch(e) {
+            console.log("HERE");
+            client.emit("error", e);
+            await db.close();
+
+            reject(e);
+        }
+    });
+
+    promise.then((data) => { return data });
+}
+
+/*
+sqlDir - SQL directory
+table - the name of the table within the desired database
+data - the new data set to replace our index
+
+** CURRENTLY ONLY SUPPORTS INTEGERS AS THE FIRST COLUMN VALUE **
+*/
+exports.setData = async function(client, sqlDir, sqlSetQuery, table, data)
+{
+    let db = new SQLite.Database(sqlDir);
+    try
+    {
+        let keys = Object.keys(data);
+        let values = Object.values(data);
+        db.serialize(function()
+        {
+            db.run((sqlSetQuery), values, function(err) {
+                if (err)
+                    client.emit("error", err.message);
+            });
+        });
+
+        await db.close();
+    }
+    catch(e)
+    {
+        client.emit("error", e);
+        return undefined;
+
+        await db.close();
+    }
+}
+
+exports.createdb = function(client, sqlDir, table, query, uniqueValue)
+{
+    return promise = new Promise((resolve, reject) =>
+    {
+        let db = new SQLite.Database(sqlDir);
+        try
+        {
+            db.serialize(function()
+        	{
+        		let sqlTable = db.prepare(`SELECT count(*) FROM sqlite_master WHERE type='table' AND name = '${table}';`);
+        		if(!sqlTable['count(*)'])
+        		{
+        			let createTbl = db.run((`CREATE TABLE IF NOT EXISTS ${table}(${query})`), function() {
+                        let createUnique = db.run((`CREATE UNIQUE INDEX IF NOT EXISTS _x${uniqueValue} ON ${table} (${uniqueValue});`), function() {
+                            sqlTable.finalize();
+                            db.close();
+                            resolve(true);
+                        });
+                    });
+        		}
+                else
+                {
+                    db.close();
+                    resolve(true);
+                }
+        	});
+        }
+        catch(e)
+        {
+            client.emit("error", e);
+            db.close();
+
+            reject(e);
+        }
+    });
+
+    promise.then((data) => { return data });
+}
+
+// **CURRENTLY NOT AVAILABLE** //
+/*
+exports.createColumn = async function(client, options, dirInfo)
+{
+    //Arg check
+    if(typeof options.name === 'undefined') throw new Error("Column name not passed as an object, 'name'");
+    if(typeof options.type === 'undefined') throw new Error("Column type not passed as an object, 'type'");
+
+    //Get how many files we would need to modify
+    if(typeof dirInfo === 'undefined')
+    {
+        await fs.readdir("./SQL/guild_data/", async (err, files) => {
+            if(err) throw err;
+
+            let dirInfo = {
+                count: files.length,
+                files: files
+            }
+            return this.createColumn(client, options, dirInfo);
+        });
+    }
+    else
+    {
+        let colName = options.name;
+        let colType = options.type;
+        for(let i = 0; i < dirInfo.count; i++)
+        {
+            try
+            {
+                let sqlFile = new SQLite(`./SQL/guild_data/${dirInfo.files[i]}/channel_settings.db3`);
+                client.createColumnData = sqlFile.prepare(`ALTER TABLE data ADD COLUMN ${colName} ${colType};`);
+                client.createColumnData.run();
+
+                sqlFile.close();
+            } catch(e) { client.emit("error", e); }
+        }
+    }
+}
+*/
+
+//Merges an array of keys, and values to be used as an object compatible with SQLite
+exports.merge = async function(keys, values)
+{
+    let obj = {};
+    for (let i = 0; i < keys.length; ++i)
+        obj[keys[i]] = values[i];
+
+    return obj;
+}
