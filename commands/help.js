adab7c02d84ffe64d5cfe51ae914bc2dff1c385c
Henok Wondasan <16henok@gmail.com>
2019-09-14 21:12:56 -0500

    Help command updated

--- commands/help.js
+++ commands/help.js
@@ -10,21 +10,27 @@ module.exports.run = async function(client, message, args)
 
     let helpMsg = new Discord.RichEmbed()
         .setColor(config.success_color)
-        .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL)
+        .setAuthor(`${client.user.username}#${client.user.discriminator}`, client.user.displayAvatarURL)
         .addField("General Commands",
         ` **\`${prefix}profile\`** - Shows your in-game profile stats.\n` +
         ` **\`${prefix}save\`** - Links your in-game profile to your account.\n` +
-        ` **\`${prefix}boxes\`** - Shows your upcoming box cycles.\n\n** **`)
+        ` **\`${prefix}boxes\`** - Shows your upcoming box cycles.\n`+
+        ` **\`${prefix}team\`** - Shows your team's info.\n\n** **`)
 
         .addField("Server Commands",
-        ` **\`${prefix}setprefix\`** - Changes your server's prefix.\n`)
+        `\***\`${prefix}setprefix\`** - Changes your server's prefix.\n\n** **`)
+
+        .addField("Misc Commands",
+        ` **\`${prefix}invite\`** - Bot invite link.\n` +
+        ` **\`${prefix}support\`** - Support server link.\n` +
+        ` **\`${prefix}info\`** - Shows info based on the bot.\n`)
 
         .addBlankField()
         .addField("Dev", `<:twitter:528807292305408030> [Henok](https://twitter.com/stupidsedits)`, true)
         .addField("Invite me", `<:discord:528808847671033867> [Invite](${config.invite_link})`, true)
         .addField("Support Server", `<:discord:528808847671033867> [Join Us](${config.server_link})`, true)
-        .setTimestamp()
-        .setFooter(`${client.user.username}#${client.user.discriminator}`, client.user.displayAvatarURL);
+
+        .setFooter("Commands marked with a * are admin only commands.");
 
     message.channel.stopTyping();
     message.reply({embed:helpMsg}).catch(err => {});
